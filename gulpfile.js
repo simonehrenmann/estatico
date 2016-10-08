8317e8f8bc8d9364309cae04507911b665b3bc24
David Simon <me@davidsimon.ch>
2016-10-08 17:30:52 +0200

    [Feature] Gulp: check node-version before running

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var validatenv = require('validate-node-version')();
+
+// Check Node Version
+if (!validatenv.satisfies) {
+	console.error(validatenv.message);
+	process.exit(1);
+}
+
 // Load tasks
 require('require-dir')('./gulp', {
 	recurse: true
